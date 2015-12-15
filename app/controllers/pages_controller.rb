318d3dd44ea70eb6dd5876928878ba207c94672d
cnosuke <shinnosuke@gmail.com>
2015-12-15 21:47:15 +0900

    Add user behavior

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
 
+  before_filter :authenticate_user!, only: %i(my)
+
   def index
+  end
+
+  def my
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
-  validates :uid, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         # :confirmable,
 
   has_many :reservations, foreign_key: :guest_id
   has_many :rooms, foreign_key: :owner_id
+
+  def owner?
+    owner
+  end
 end
